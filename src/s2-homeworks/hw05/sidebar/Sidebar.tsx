695cb9ddfef309fefe41a1ecf13b3b6569732c14
1Molot <sinkevich1990@gmail.com>
2023-03-31 20:41:19 +0300

    fix:hw5-2pop

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -32,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={({ isActive })  => isActive ? s.active : s.navLink}
+                        className={({ isActive })  => isActive ? s.active + " " + "active" : s.navLink}
                     >
                         Pre-junior
                     </NavLink>
@@ -41,7 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={({ isActive }) => isActive ? s.active : s.navLink}
+                        className={({ isActive }) => isActive ? s.active + " " + "active" : s.navLink}
                     >
                         Junior
                     </NavLink>
@@ -50,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={({ isActive }) => isActive ? s.active : s.navLink}
+                        className={({ isActive }) => isActive ? s.active  + " " + "active" : s.navLink}
                     >
                         Junior Plus
                     </NavLink>
