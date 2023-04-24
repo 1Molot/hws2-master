30c4cbc0e09313d26bc1fe0e1a74456d206be818
1Molot <sinkevich1990@gmail.com>
2023-04-24 17:46:12 +0300

    fix:HW9-3

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
+import {format} from "util";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
@@ -40,8 +41,8 @@ function Clock() {
     const stringDate = date.toLocaleDateString('ru-Ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.toLocaleDateString("en-US") || <br/> // пишут студенты date
-    const stringMonth = date.toLocaleDateString("en-US") || <br/> // пишут студенты month
+    const stringDay =  `${new Intl.DateTimeFormat('en-US', { weekday: 'long'}).format(date)}` || <br/>
+    const stringMonth =  `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date)}`   || <br/> 
 
     return (
         <div className={s.clock}>
