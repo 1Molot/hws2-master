62197c7fb84b06652b7cdedec75547c745384d88
1Molot <sinkevich1990@gmail.com>
2023-06-02 16:48:53 +0300

    fix:HW14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -16,7 +16,7 @@ import {useSearchParams} from 'react-router-dom'
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+   'https://samurai.it-incubator.io/api/3.0/homework/test2',
             {params: {find}}
         )
         .catch((e) => {
@@ -34,7 +34,12 @@ const HW14 = () => {
         setLoading(true)
         getTechs(value)
             .then((res) => {
+                if(res){
+                    setTechs(res.data.techs)
+                }
+
                 // делает студент
+
 
                 // сохранить пришедшие данные
 
@@ -45,10 +50,9 @@ const HW14 = () => {
     const onChangeText = (value: string) => {
         setFind(value)
         // делает студент
-
         // добавить/заменить значение в квери урла
         // setSearchParams(
-
+        setSearchParams({find:value})
         //
     }
 
